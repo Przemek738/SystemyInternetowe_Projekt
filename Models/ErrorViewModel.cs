6a74724ad05361014612230cf6ba7c9bbf8c7676
Przemek738 <przemyslaw.pyrzynski@studenci.collegiumwitelona.pl>
2026-05-25 16:27:47 +0200

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ArcadeProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
